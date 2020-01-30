e736a8d427f3db9f40b184561beab4ad28d8e030
Alehandro Persaud <andrew041994@gmail.com>
2020-01-30 01:54:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,10 @@ attr_accessor :name, :artist, :genre
   @@count = 0
 
      def initialize(name, artist, genre)
-       @@count+=1
-       @name=name
+       @@count += 1
+       @name = name
+       @artist = artist
+       @genre = genre
      end
      #
     #  def name=(name)
