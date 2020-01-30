b9fb8ee9540ddeadd53069d8d4671ee7773745f8
Alehandro Persaud <andrew041994@gmail.com>
2020-01-30 02:17:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ attr_accessor :name, :artist, :genre
      def self.genre_count
        genre_count = {}
        @@genres.each do |i|
-         if genre_count.include? i
+         if genre_count.include i
           genre_count[i] += 1
          else
            genre_count[i] = 1
