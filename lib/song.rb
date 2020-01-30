d11d0969e51439e540988255e45717b5bdeee360
Alehandro Persaud <andrew041994@gmail.com>
2020-01-30 01:52:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,11 @@ attr_accessor :name :artist :genre
        @@count+=1
 
      end
-     # 
+     #
     #  def name=(name)
     #    @name = name
     #  end
-     # 
+     #
     #  def name
     #    @name
     #  end
